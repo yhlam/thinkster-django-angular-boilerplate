6ef8f2eabe3960b2818820fedb60d4a51e341c30
Hei <lamyuenhei@gmail.com>
2014-11-20 15:33:15 +0800

    Setup admin URL

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     '',
+
+    url(r'^admin/', include(admin.site.urls)),
 
     url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 )

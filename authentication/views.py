52fe5ce0964a53ed4b61befb477633fd0364be18
Hei <lamyuenhei@gmail.com>
2014-11-22 17:08:56 +0800

    Add account API

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Account
+from .serializers import AccountSerializer
+from .permissions import IsAccountOwner
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_permission(self):
+        method = self.request.method
+        if (method in permissions.SAFE_METHODS or method == 'POST'):
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,10 +2,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+from rest_framework import routers
+
+from authentication.views import AccountViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'account', AccountViewSet)
+
+
 urlpatterns = patterns(
     '',
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 )
